4415620b9c0095d50b3f3c5cf6191cce28c5c423
Fede <fede.n.gal@hotmail.com>
2020-03-07 22:36:50 -0300

    delete some things

--- src/game.js
+++ src/game.js
@@ -1,7 +1,4 @@
 import { GameplayGraphics } from '../engine/rendering.js';
-import Hero from './hero.js';
-import { exampleRender } from '../engine/tilemap.js';
-import { exampleZone, exampleWorld } from './world.js';
 
 const ArrayNewFunctionalities = {
   removeIf(condition) {
@@ -22,7 +19,6 @@ let showGrid = false;
 const { renderer, screen } = GameplayGraphics;
 
 let count;
-const enemies = [];
 
 function initialState() {
   renderer.fillStyle = 'orange';
@@ -31,36 +27,6 @@ function initialState() {
 }
 
 window.addEventListener('resize', initialState);
-
-const hero0 = new Hero(0.47);
-const hero1 = new Hero(0.5, undefined, 0.1, undefined);
-const hero2 = new Hero(undefined, 20, 2);
-const hero = hero0;
-
-class Enemy {
-  constructor(x = GameplayGraphics.screen.width, y = 0, up = false) {
-    this.x = x;
-    this.y = y;
-    this.up = up;
-    this.width = Enemy.size.width;
-    this.height = Enemy.size.height;
-  }
-
-  static get size() {
-    return { width: GameplayGraphics.tileSize.w, height: GameplayGraphics.tileSize.h };
-  }
-}
-
-
-const defaultValue = undefined;
-
-function renderEnemy({ x, y, up = false }) {
-  renderer.fillStyle = 'red';
-  renderer.renderFullRectangle(x, y - up * Enemy.size.height, Enemy.size.width, Enemy.size.height * 2);
-}
-
-let up = false;
-const offset = 0;
 const camera = { x: 0, y: 0 };
 
 export default {
@@ -68,41 +34,6 @@ export default {
     initialState();
   },
   update() {
-    hero.update();
-
-    const lapse = 55;
-    if (count % lapse === 0) {
-      up = !up;
-
-      enemies.push(new Enemy(
-        // defaultValue, GameplayGraphics.tileSize.h * Math.round(Math.random() * (GameplayGraphics.screen.height / GameplayGraphics.tileSize.h)),
-        // defaultValue, GameplayGraphics.tileSize.h * (Math.round(count / lapse / 2) % 2 === 0 ? 3 : 5),
-        defaultValue, GameplayGraphics.tileSize.h * 3, up,
-      ));
-    }
-
-    enemies.forEach((e) => { e.x--; });
-    enemies.removeIf(e => e.x + Enemy.size.width < 0);
-
-    // // Temporal pseudo-collision with screen's bottom bound
-    // if (hero.y > screen.height - hero.height) {
-    //   hero.y = screen.height - hero.height;
-    //   hero.velocityVector = 0;
-    // }
-
-    const speed = 1;
-    const cameraGap = 5;
-
-    hero.x += speed;
-    camera.x += speed;
-    if (hero.y - camera.y < cameraGap) {
-      camera.y = hero.y - cameraGap;
-    }
-
-    if (camera.y + screen.height - (hero.y + hero.height) < cameraGap) {
-      camera.y = hero.y + hero.height + cameraGap - screen.height;
-    }
-
     ++count;
   },
   render() {
@@ -110,38 +41,11 @@ export default {
     if (showGrid) {
       renderer.renderTileGrid();
     }
-    // renderer.renderFullRectangle(0, Enemy.size.height, GameplayGraphics.screen.width, Enemy.size.height);
-    // renderer.renderFullRectangle(0, Enemy.size.height * 5, GameplayGraphics.screen.width, Enemy.size.height);
-    // enemies.forEach(renderEnemy);
-    // exampleRender(offset, offset -= 0.25);
-    // exampleZone.render(camera);
-    exampleWorld.render(camera);
-    hero.render(camera);
-
-
-    const { renderingContext2D, canvasHeight } = GameplayGraphics;
-    const prevColor = renderingContext2D.fillStyle;
-    renderingContext2D.fillStyle = 'yellow';
-    const fontSize = 18;
-    renderingContext2D.font = `bold ${fontSize}px arial`;
-    renderingContext2D.fillText(`camera: { ${camera.x}, ${camera.y} }`, 10, 20, 160);
-    renderingContext2D.fillStyle = prevColor;
+    // renderer.renderSprite()
   },
   pressed: {
-    Space() {
-      hero.propulse();
-    },
-    ScreenTouch() {
-      hero.propulse();
-    },
   },
   released: {
-    Space() {
-      hero.fall();
-    },
-    ScreenTouch() {
-      hero.fall();
-    },
   },
   fired: {
     KeyD() {
